6393f3229d42d057a1d9a31ae4f1e53afcf7eefc
szauer-coder <szauerdavid5@gmail.com>
2025-10-28 23:40:47 +0100

    Megoldottam mindent

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
+const eloHivo = document.querySelector(".inputValue");
+const hozzaAdGomb = document.querySelector(".hozzaAdGomb");
+const iconka = document.querySelector(".iconka");
+const plusgomb = document.querySelector(".Plus");
+const minusgomb = document.querySelector(".Minus");
+const input = document.getElementById("taskInput");
+const addButton = document.getElementById("addTextButton");
+const todoList = document.querySelector(".nemKesz");
 
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-const eloHivo = document.querySelector(".inputValue")
-const hozzaAdGomb = document.querySelector(".hozzaAdGomb")
-const iconka = document.querySelector(".iconka")
-const plusgomb = document.querySelector(".Plus")
-const minusgomb = document.querySelector(".Minus")
+function renderTasks(){
+    todoList.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const div = document.createElement("div");
+        const h3 = document.createElement("h3");
+        div.classList.add("feladat");
+        h3.textContent = task;
+        todoList.appendChild(div);
+        div.appendChild(h3);
+    });
+}
 
+addButton.addEventListener("click", () => {
+    const newTask = input.value.trim();
 
+    if(newTask){
+        tasks.push(newTask);
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+            input.value = "";
+            renderTasks();
+    }
+});
+
+renderTasks();
 
 function generateId() {
   return "task-" + Date.now();
@@ -44,7 +70,7 @@ function hozzaAdas(){
     const ertek = eloHivo.value.trim();
     if (ertek === "") return;
 
-    const nemkesz = document.getElementById("faszomategyed")
+    const nemkesz = document.getElementById("hozzaadas")
 
     const id = generateId();
 
